86c817602c6cfe683d6326b0d3c3403bc4c7a428
bikashsamantaray <samantaraybikash2015@gmail.com>
2022-09-03 00:39:32 +0530

    ui

--- app/src/main/java/com/bikash/weather/MainActivity.kt
+++ app/src/main/java/com/bikash/weather/MainActivity.kt
@@ -98,7 +98,8 @@ class MainActivity : AppCompatActivity() {
                 ) {
                     if (response.isSuccessful){
                         hideProgressDialog()
-                        val weatherList: WeatherResponse? = response.body()
+                        val weatherList: WeatherResponse = response.body()!!
+                        setupUI(weatherList)
                         Log.i("Response Result", "$weatherList")
                     }else{
                         hideProgressDialog()
@@ -193,6 +194,23 @@ class MainActivity : AppCompatActivity() {
         if (mProgressDialog != null){
             mProgressDialog!!.dismiss()
         }
+    }
+
+    private fun setupUI(weatherList: WeatherResponse){
+        for( i in weatherList.weather.indices){
+            Log.i("Weather name", weatherList.weather.toString())
+            //tv_main.text = weatherList.weather[i].main
+            //tv_main_description.text = weatherList.weather[i].description
+            //tv_temp.text = weatherList.main.temp.toString() + getUnit(application.resources.configuration.locales.toString())
+        }
+    }
+
+    private fun getUnit(value: String):String?{
+        var value = "°C"
+        if("US" == value || "LR" == value || "MM" == value){
+            value = "°F"
+        }
+        return value
     }
 
 }
--- app/src/main/java/com/bikash/weather/network/WeatherService.kt
+++ app/src/main/java/com/bikash/weather/network/WeatherService.kt
@@ -7,12 +7,12 @@ import retrofit2.http.Query
 
 interface WeatherService {
 
-    @GET("2.5/weather")
+    @GET("api.openweathermap.org/data/2.5/weather")
     fun getWeather(
         @Query("lat") lat:Double,
         @Query("lon") lon:Double,
         @Query("units") units:String?,
-        @Query("appid") appid:String?,
+        @Query("appid") appid:String?
 
     ): Call<WeatherResponse>
 }
